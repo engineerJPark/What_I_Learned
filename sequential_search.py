d0c23852a3acd096af1e00908326f6443005375a
engineerJPark <71544293+engineerJPark@users.noreply.github.com>
2020-09-19 20:01:54 +0900

    Update sequential_search.py

--- sequential_search.py
+++ sequential_search.py
@@ -1,7 +1,7 @@
 def search_list(lst, findingvalue):
     n = len(lst)
     for i in range(0, n):
-        if findingvalue == lst[a]:
+        if findingvalue == lst[i]:
             return i
     
     return -1
@@ -10,3 +10,5 @@ lst_v = [17,92,18,33,58,7,33,42]
 print(search_list(lst_v, 18))
 print(search_list(lst_v, 33))
 print(search_list(lst_v, 900))
+
+//이런 순차탐색의 경우 보수적으로 계산 복잡도를 정한다. ,O(n)이다.
