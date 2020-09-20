7409c57eb1005ad186ecdc2da74b6ed04839c42a
engineerJPark <71544293+engineerJPark@users.noreply.github.com>
2020-09-20 16:51:54 +0900

    Create insert_sorting(삽입정렬).py
    중간 실력테스트용으로 만든 삽입정렬 알고리즘이다.

---
+++ insert_sorting(삽입정렬).py
@@ -0,0 +1,22 @@
+#삽입정렬 예제이다.
+
+#먼저 연습
+
+def insert_sort(lst):
+    l = len(lst)
+    i = 0
+    lst_sort = []
+    lst_sort.append(lst[i])
+    for i in range(0,l):
+        m = len(lst_sort)
+        for j in range(0, m):
+            if lst[i] < lst_sort[j]:
+                lst_sort.insert(j, lst[i])
+                break
+            #맨 뒤까지 다 찾아봤으나 안보이는 경우 맨 뒤에 쳐박아야한다.
+            if (j == (m-1)) and (lst[i] not in lst_sort) :
+                lst_sort.append(lst[i])
+    return lst_sort
+
+d = [3,1,5,2,4]
+print(insert_sort(d))
